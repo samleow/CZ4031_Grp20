441f2729c4c1dade4feab04432e9bc935e9a3d03
Samuel Leow <samuel.leow@gmail.com>
2021-09-23 02:10:49 +0800

    testing some stuff

--- CZ4031_p1/CZ4031_p1.cpp
+++ CZ4031_p1/CZ4031_p1.cpp
@@ -1,8 +1,16 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+#define DISK_SIZE           100000000
+#define BLOCK_SIZE          100
+#define BLOCKS_IN_DISK      DISK_SIZE/BLOCK_SIZE
+#define RECORD_SIZE         sizeof(Record)
+#define RECORDS_PER_BLOCK   (BLOCK_SIZE-sizeof(int))/RECORD_SIZE
 
 struct Record
 {
@@ -21,12 +29,8 @@ struct Disk_Block
     // header includes id and record length
     // not sure if can omit header
     int id;
-    int record_len;
 
-    // using vector as a variable length list
-    // preferred to use array
-    // need recheck Disk_Block total size, coz vector may affect size
-    vector<Record> records;
+    Record records[RECORDS_PER_BLOCK];
 };
 
 // block_size = 100B / 500B
@@ -45,34 +49,78 @@ struct Disk_Block
 // 
 // As for disk memory simulated on main memory (experiment 1), we can just save to stack
 
+int getTotalRecordCount() {
+
+    ifstream myfile;
+    myfile.open("data.tsv");
+
+    string line;
+    int line_count = 0;
+    bool skippedFirstLine = false;
+
+    while (getline(myfile, line))
+    {
+        if (!skippedFirstLine)
+            skippedFirstLine = true;
+        else {
+            line_count++;
+        }
+    }
+
+    myfile.close();
+    return line_count;
+}
+
+void retrieveData(Disk_Block *disk)
+{
+    ifstream myfile;
+    myfile.open("data.tsv");
+
+    string line;
+    bool skippedFirstLine = false;
+
+    while (getline(myfile, line))
+    {
+        if (!skippedFirstLine)
+            skippedFirstLine = true;
+        else
+        {
+            //string id = line.substr(2,7);
+            cout << line << endl;
+            break;
+        }
+    }
+
+    myfile.close();
+
+}
+
 int main()
 {
 
-    // disk capacity (in MB)
-    // 100 - 500
-    int disk_cap = 100;
-    cout << "Disk capacity:\t" << disk_cap << "MB" << endl;
+    //Disk_Block Disk[BLOCKS_IN_DISK];
 
-    // block size (in B)
-    // should get input for block size 100 or 500
-    int block_size = 100;
-    cout << "Block size:\t" << block_size << "B"  << endl;
+    // disk capacity
+    cout << "Disk capacity:\t" << DISK_SIZE << "B" << endl;
+
+    // block size
+    cout << "Block size:\t" << BLOCK_SIZE << "B"  << endl;
 
     // number of blocks in the disk
-    int blocks_in_disk = (disk_cap * pow(10,6)) / block_size;
-    cout << "Blocks in disk:\t" << blocks_in_disk << endl;
+    cout << "Blocks in disk:\t" << BLOCKS_IN_DISK << endl;
 
-    cout << "Record size:\t" << sizeof(Record) << "B"  << endl;
+    cout << "Record size:\t" << RECORD_SIZE << "B"  << endl;
 
     // this is not accounting for header size !!
     // not sure if correct or not !!
     // if accounting for header size, need to be = floor((block_size - sizeof(Disk_Block.id) - sizeof(Disk_Block.record_len)) / sizeof(t))
-    int records_per_block = floor(block_size / sizeof(Record));
-    cout << "Num of records in a block:\t" << records_per_block << endl << endl;
+    cout << "Num of records in a block:\t" << RECORDS_PER_BLOCK << endl << endl;
 
     // read from data file here onwards
     // Get data and store to "disk"
     // need count number of records to store and check if disk capacity can hold all data
+
+    cout << getTotalRecordCount() << endl << endl;
 
 #pragma region testing block and record init
 
@@ -80,23 +128,19 @@ int main()
     // for now testing with only one block
     Disk_Block b1;
     b1.id = 1;
-    b1.record_len = sizeof(Record);
-    b1.records.resize(records_per_block);
+    
 
-    for (int i=0; i<records_per_block; i++)
+    for (int i=0; i<RECORDS_PER_BLOCK; i++)
     {
         // dummy values
-        Record r = {i, 1.5*i, 2*i};
+        Record r = {i, 1.5*i+1, 2*i+1};
         // this is passing by value
         // will be good to pass by reference
         b1.records[i] = r;
     }
     cout << "Block 1 size: " << sizeof(b1) << "B" << endl;
-    cout << "vector size: " << sizeof(vector<Record>) << "B" << endl;
-    cout << "Block 1 vector size: " << sizeof(Record)*b1.records.size() << "B" << endl;
-    cout << "Block 1 actual size: " << sizeof(b1) + sizeof(Record) * b1.records.size() << "B" << endl << endl;
 
-    for (int i=0; i<b1.records.size(); i++)
+    for (int i=0; i<RECORDS_PER_BLOCK; i++)
     {
         cout << "Block 1 record " << b1.records[i].id << "'s avg rating:\t\t" << b1.records[i].avg_rating << endl;
         cout << "Block 1 record " << b1.records[i].id << "'s num of votes:\t" << b1.records[i].num_of_votes << endl << endl;
